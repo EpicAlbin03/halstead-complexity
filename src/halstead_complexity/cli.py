7175c3940555e2cedf9f871a4b47e5792861b8e9
EpicAlbin03qqqq <github.garter593@passmail.net>
2025-10-02 15:50:06 +0200

    config

--- src/halstead_complexity/cli.py
+++ src/halstead_complexity/cli.py
@@ -1,16 +1,33 @@
+from pathlib import Path
 from typing import Optional
 
 import typer
 
-from halstead_complexity import __app_name__, __version__
+from halstead_complexity import ERRORS, __app_name__, __version__, config
 
 app = typer.Typer()
+
+
+@app.command()
+def init() -> None:
+    """Initialize the config."""
+    app_init_error = config.init_app()
+    if app_init_error:
+        typer.secho(
+            f'Creating config file failed with "{ERRORS[app_init_error]}"',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    else:
+        typer.secho(
+            f"Config file created successfully {Path(typer.get_app_dir(__app_name__))}",
+            fg=typer.colors.GREEN,
+        )
 
 
 def _version_callback(value: bool) -> None:
     if value:
         typer.echo(f"{__app_name__} v{__version__}")
-
         raise typer.Exit()
 
 
