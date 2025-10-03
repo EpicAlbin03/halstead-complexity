144ceefcd3e66eb866fa6131f034a6dc353d8648
EpicAlbin03qqqq <github.garter593@passmail.net>
2025-10-03 14:20:53 +0200

    redo

--- src/halstead_complexity/__init__.py
+++ src/halstead_complexity/__init__.py
@@ -1,13 +1,2 @@
 __app_name__ = "halstead-complexity"
 __version__ = "0.1.0"
-
-(
-    SUCCESS,
-    DIR_ERROR,
-    FILE_ERROR,
-) = range(3)
-
-ERRORS = {
-    DIR_ERROR: "config directory error",
-    FILE_ERROR: "config file error",
-}
